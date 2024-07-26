ba2c583e849da09c6e6c136bfe6ec73797d1208c
Mayssa <mayssa.labidi.94@gmail.com>
2024-07-26 12:35:20 +0200

    Commiting Scala Basics Part

--- Scala_Training_Exercices/src/Basics/Recursion.scala
+++ Scala_Training_Exercices/src/Basics/Recursion.scala
@@ -1,5 +1,7 @@
 package Basics
 
+import scala.annotation.tailrec
+
 object Recursion  extends App {
 
   def factorial(n:Int) : Int = {
@@ -14,10 +16,98 @@ object Recursion  extends App {
 
   println(factorial(10))
 
-  /**   println(factorial(50000)) ==> Crash with .StackOverflowError that happen because the recursive depth is too big
+  /**   println(factorial(50000)) ==> Crash with StackOverflowError that happen because the recursive depth is too big and the stack is limited
    *Solution : write code in a smart way
    */
 
 
+  def anotherFactorial (n:Int) : BigInt = {
+    @tailrec
+    def factHelper(x:Int, accumulator : Int) : BigInt = {
+    if (x <= 1) accumulator
+    else   factHelper(x-1, x*accumulator) //TAIL RECURSION = use recursive call as the LAST expression
 
+    }
+    factHelper(n,1)
+  }
+  /*
+  anotherFactorial(10) = factHelper(10,1)
+                       = factHelper(9,10*1)
+                       = factHelper(8,9* 10*1)
+                       = factHelper(7,8* 9*10*1)
+                       = ...
+                       = factHelper(2,3* 4*5*6*7*8*9*10*1)
+                       = factHelper(1,2* 3*4*5*6*7*8*9*10*1)
+                       = 2* 3*4*5*6*7*8*9*10*1)
+   */
+  anotherFactorial(50000)
+
+  /**
+   * ==> Scala in this case preserve the same stack frame and not use additionnal stack frames for recursive calls
+   * in the previous implementation Scala needed a recursive call stack frame for each recursive call,so that it computes the intermediate result
+   * ==> When YOU NEED LOOPS, Use _TAIL_RECURSION
+   */
+
+  /**
+   * 1. Concatenate a String n times
+   * 2. IsPrime function tail recursive
+   * 3. Fibonacci function, tail recursive
+   */
+
+
+def concatenateTailrec(aString : String, n: Int) : String = {
+
+  @tailrec
+  def Helper(x:Int, s : String ,accumulator : String):String = {
+    if(x<=1) s+accumulator
+    else Helper(x-1,s,accumulator + s)
+  }
+
+  Helper(n,aString,"")
+}
+  println(concatenateTailrec("Hello", 10))
+
+def isPrimeTailREc(n:Int): Boolean = {
+
+  /* Basic Recursion version
+  def div(x:Int) : Boolean = {
+    if (x <= 1) true
+    else n % x != 0 && div(x - 1)
+  }
+  val d = n/2
+  div(d)
+
+   */
+  @tailrec
+  def div(x:Int, isStillPrime : Boolean):Boolean = {
+    if(!isStillPrime) false
+    else if(x<= 1) true
+    else div(x-1, isStillPrime && n%x!=0)
+  }
+  val d = n/2
+  div(d,true)
+
+}
+  println(isPrimeTailREc(2003))
+  println(isPrimeTailREc(629))
+
+ /* Basic Recursion version */
+  def fibonacci(n: Int): Int = {
+    if (n <= 2) 1
+    else fibonacci(n - 1) + fibonacci(n - 2)
+  }
+
+  /* New Tail REC fibonacci function */
+
+  def fibonacciTailRec(n:Int) : Int = {
+   @tailrec
+    def Helper(x: Int, last : Int, nextToLast : Int) : Int = {
+    if(x>=n)  last
+    else Helper(x+1, last +nextToLast, last )
+    }
+    if(n<=2) 1
+    else
+      Helper(2,1,1)
+  }
+println(fibonacciTailRec(8))
 }
--- Scala_Training_Exercices/src/Basics/ValuesVariablesType.scala
+++ Scala_Training_Exercices/src/Basics/ValuesVariablesType.scala
@@ -23,4 +23,26 @@ object ValuesVariablesType  extends  App{
 
   val aString : String = "Hello"
   val anotherString ="goodbye"
+
+
+  /**
+   *   Instruction Vs Expressions
+   *   Instructions are doing Something , tell the computer to do in imperative languages like Java or Python, you work with instructions
+   *   Expressions are something that has a value AND OR a type
+   *   In Scala abd in functional programming in general, we'll think in term of expressions
+    */
+//IF expression
+  val aCondition = true
+  val aConditionValue = if(aCondition) 5 else 3
+  println(aConditionValue)
+  println(if(aCondition) 5 else 3)
+
+// LOOPS !! very specific to imperative programming not Scala
+  var i = 0
+  while(i<10) {
+    println(i)
+    i+=1
+  }
+   //!!! Never write  this again, do not use loops in Scala code
+   // EVERYTHING In scala is an Expression
 }
